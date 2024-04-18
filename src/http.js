aa85cc77320e891b38269724d766348d2693845b
unknown <lukacc008@gmail.com>
2024-04-18 18:47:59 +0200

    Sending data with POST to my dummy backend

--- src/http.js
+++ src/http.js
@@ -1,4 +1,4 @@
- export async function fetchAvailablePlaces() {
+export async function fetchAvailablePlaces() {
   const response = await fetch("http://localhost:3000/places");
   const resData = await response.json();
 
@@ -7,4 +7,22 @@
   }
 
   return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+
+  return resData.message;
 }
